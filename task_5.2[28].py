b5aede36a86d25ee87023b96f9da329dac79c8ae
Александр <femt0dead3@mail.ru>
2023-03-27 20:53:28 +0300

    end of fourteenth homework

---
+++ task_5.2[28].py
@@ -0,0 +1,11 @@
+# 5.2[28]: Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел. Из всех арифметических операций допускаются только +1 и -1. Циклы использовать нельзя
+# Примеры/Тесты:
+# <function_name>(0,0) -> 0
+# <function_name>(0,2) -> 2
+# <function_name>(3,0) -> 3
+
+def sum_number(first_number, second_number):
+    if first_number == 0:
+        return second_number
+    return sum_number(first_number-1, second_number)+1
+print(sum_number(12,11))
